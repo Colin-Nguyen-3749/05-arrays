5ae08d677ace7dcb77bbb5cd262ce8e2974681f5
Colin James Nguyen <nguyen_c9@denison.edu>
2025-06-30 21:44:11 +0000

    Added push method to add movies to movie list

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-// Comedy movies array
-const comedyMovies = [
-  "The Mask",
-  "Dumb and Dumber",
-  "Superbad",
-  "Home Alone"
-];
-
-// Action movies array
-const actionMovies = [
-  "Die Hard",
-  "Mad Max: Fury Road",
-  "John Wick",
-  "The Avengers"
-];
-
-// Drama movies array
-const dramaMovies = [
-  "Forrest Gump",
-  "The Shawshank Redemption",
-  "The Godfather",
-  "A Beautiful Mind"
-];
-
-// Sci-Fi movies array
-const sciFiMovies = [
-  "Inception",
-  "The Matrix",
-  "Interstellar",
-  "Star Wars"
-];
-
 // Set up event listener for when the user selects a genre
 document.getElementById("genre").addEventListener("change", function() {
   // Get the selected genre
   const selectedGenre = document.getElementById("genre").value;
 
   // Create an empty array for the movie list
+  let movieList = [];
+
+  // Push movie titles directly to movieList based on the selected genre
+  if (selectedGenre === "Comedy") {
+    // Add Comedy movies
+    movieList.push("The Mask");
+    movieList.push("Dumb and Dumber");
+    movieList.push("Superbad");
+    movieList.push("Home Alone");
+  } else if (selectedGenre === "Action") {
+    // Add Action movies
+    movieList.push("Die Hard");
+    movieList.push("Mad Max: Fury Road");
+    movieList.push("John Wick");
+    movieList.push("The Avengers");
+  } else if (selectedGenre === "Drama") {
+    // Add Drama movies
+    movieList.push("Forrest Gump");
+    movieList.push("The Shawshank Redemption");
+    movieList.push("The Godfather");
+    movieList.push("A Beautiful Mind");
+  } else if (selectedGenre === "Sci-Fi") {
+    // Add Sci-Fi movies
+    movieList.push("Inception");
+    movieList.push("The Matrix");
+    movieList.push("Interstellar");
+    movieList.push("Star Wars");
+  }
+
+  // Display the list of movies on the page
+  document.getElementById("movieRecommendations").innerText = `Enjoy: ${movieList.join(", ")}!`;
+});
   let movieList = [];
 
   // Assign the correct movie titles to movieList based on the selected genre
